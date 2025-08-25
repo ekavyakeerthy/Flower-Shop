1a7829ad4805314a538d82c12014c55bcbf017ea
kavyakeerthy <ekaviyakeerthi@gmail.com>
2025-08-25 14:45:38 +0530

    fix: correct index.html script path and vite base config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+'@vitejs/plugin-react'
 
 export default defineConfig({
   base: "./",
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['lucide-react']
-    }
-  }
+    
 })
 
     
